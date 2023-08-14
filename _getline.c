f0d2befc6f41938288ab5e44601cebdfafebc7f2
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2023-08-14 08:07:37 -0700

    TRY

--- _getline.c
+++ _getline.c
@@ -13,18 +13,33 @@
 
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-	int fd;
+	int fd , len = 0;
 	ssize_t nread;
 	size_t num;
 
 	if(*lineptr == NULL)
 	{
-		num = 6;
-		*lineptr = malloc(sizeof(char *) * num);
-		*n = num;
+		*n = 3;
+		*lineptr = malloc(sizeof(char *) * ((*n) + 1));
 	}
 	fd = fileno(stream);
 	nread = read(fd, *lineptr, *n);
+	/**lineptr[nread] = '\0';*/
+	while (*lineptr[len])
+	{
+		printf("%d:%d\n", len, *lineptr[len]);
+		len++;
+	}
+
+	/*while(*lineptr[(nread + len) - 1] != '\n')
+	{
+		printf("%c\n", *lineptr[(nread + len) - 1]);
+		*n += 3;
+		len += nread;
+		*lineptr = realloc(*lineptr, *n);
+		nread = read(fd, *lineptr + nread, ((*n) - nread));
+	}
+	len += nread;*/
 	if (nread == 0)
 	{
 		nread = -1;
